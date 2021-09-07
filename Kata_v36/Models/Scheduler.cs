5d7f09f4369f996b1afbb061277d190fab514d83
Hasan Jamous <hasan.jamous@learnet.se>
2021-09-07 10:42:30 +0200

    commit test

--- Kata_v36/Models/Scheduler.cs
+++ Kata_v36/Models/Scheduler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,7 @@ namespace Scheduler.Models
                 Applicant applicant = new Applicant();
                 applicant.Name = randomNames[i];
                 UnassignedApplicants.Add(applicant);
+                Debug.WriteLine("Applicant namne: " + applicant.Name);
             }
 
             for (int i = 0; i < 4; i++)
